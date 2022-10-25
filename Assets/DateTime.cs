bb9fa72ab7c79ed6d03663655c6d6b11d4d6da1f
Shahbinshoaib <47353965+Shahbinshoaib@users.noreply.github.com>
2022-10-25 22:56:48 +0500

    Initial Commit

---
+++ Assets/DateTime.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Globalization;
+using System;
+
+
+public class DateTime : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(System.DateTime.Now);
+    }
+}
